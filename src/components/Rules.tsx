4bad2facd7feddb65bc4b6ffc0201e99020db5d5
oluwatobicode <odetokuntreasure6@gmail.com>
2025-08-31 23:55:03 +0100

    rules page in progress & context api & state setup

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -1,8 +1,49 @@
+const rules = [
+  {
+    id: 1,
+    name: "Choose a category",
+    rule: "First, choose a word category, like animals or movies. The computer then randomly selects a secret word from that topic and shows you blanks for each letter of the word.",
+  },
+  {
+    id: 2,
+    name: "Guess letters",
+    rule: "Take turns guessing letters. The computer fills in the relevant blank spaces if your guess is correct. If it’s wrong, you lose some health, which empties after eight incorrect guesses.",
+  },
+  {
+    id: 3,
+    name: "Win or lose",
+    rule: "You win by guessing all the letters in the word before your health runs out. If the health bar empties before you guess the word, you lose.",
+  },
+];
+
 const Rules = () => {
   return (
-    <div>
-      <h1>Rules for the games</h1>
-    </div>
+    <main className="min-h-screen bg-[url('/images/background-mobile.svg')] md:bg-[url('/images/background-desktop.svg')] lg:bg-[url('/images/background-desktop.svg')] bg-cover md:bg-cover bg-center bg-no-repeat flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-[48px] w-full text-center md:text-[104px] lg:text-[136px] font-medium bg-gradient-to-b from-[#67B6FF] to-to-white bg-clip-text leading-[120%] text-stroke -tracking-[0.5%]">
+          How to Play
+        </h1>
+
+        <div className="flex flex-row gap-[25px] mt-5">
+          {rules.map((el) => (
+            <div
+              key={el.id}
+              className="lg:w-[384px] lg:h-[550px] flex flex-col items-center justify-center space-y-3 rounded-[40px] bg-white"
+            >
+              <div className="text-center space-y-4 p-4">
+                <h1 className="text-[88px] leading-[120%] text-[#2463FF]">
+                  0{el.id}
+                </h1>
+                <h1 className="text-[48px] text-[#261676]">{el.name}</h1>
+                <p className="text-[26px] leading-[120%] tracking-[5%] text-[#887DC0]">
+                  {el.rule}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
   );
 };
 export default Rules;
--- src/contexts/GameProvider.tsx
+++ src/contexts/GameProvider.tsx
@@ -1,4 +1,11 @@
-import React, { createContext, useContext, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  type Dispatch,
+  type ReactNode,
+  type SetStateAction,
+} from "react";
 
 /* 
 
@@ -8,27 +15,156 @@ so lets say if I have a hidden word such as the lion king i am to try to get
 that hidden word with as little number of tries possible
 
 interface GameState {
-hiddenWord: String
-MatchedWord: String
-life: 10
-compredWord: String
-slectedCategory: String
+  secretWord: string;
+  setSecretWord: Dispatch<SetStateAction<string>>;
+  category: string;
+  selectedCategory: Dispatch<SetStateAction<string>>;
+  showMenu: boolean;
+  setShowMenu: Dispatch<SetStateAction<boolean>>;
+}
+
+interface GameActionProps {
+  children: ReactNode;
+  selectAlphabet: () => void;
+  playGame: () => void;
+  pauseGame: () => void;
+  reduceHealthPoints: () => void;
+  playAgain: () => void;
+  continueGame: () => void;
+  newCategory: () => void;
+  quitGame: () => void;
 }
 
 
+FLOW OF THE APP 
+- when the user clicks on play, he is taken to page called select category 
+- if the user then selects a category from [movies, tv shows, countries, capital cities, animals , sports]
+- each of the category has a like a bucket of words, which is randomized and a word is given 
+- the length of the word is shown in terms of positions with spacing
+- if the user selects an alphabet and the alphabet is in that word it will show in the position of that word 
+- if the user selects an alphabet and the alphabet is not in that word it will not show and a point would be removed from their health
+- if the user cannot guess the full word they loose 
+- if the user finishes guessing the full word they win
+
+
+
+questions I have to ask myself
+0. how will i get a secret word from the category selected ?
+My best guess: 
+if the user selects a category lets say animals it will go to the animals object and randomize the array of objects there and return a secret word!
+
+"Animals": [
+            {"name": "Elephant", "selected": false},
+            {"name": "Lion", "selected": false},
+            {"name": "Giraffe", "selected": false},
+            {"name": "Penguin", "selected": false},
+            {"name": "Dolphin", "selected": false},
+            {"name": "Tiger", "selected": false},
+            {"name": "Kangaroo", "selected": false},
+            {"name": "Panda", "selected": false},
+            {"name": "Zebra", "selected": false},
+            {"name": "Polar Bear", "selected": false},
+            {"name": "Cheetah", "selected": false},
+            {"name": "Rhino", "selected": false},
+            {"name": "Buffalo", "selected": false},
+            {"name": "Koala", "selected": false},
+            {"name": "Gorilla", "selected": false},
+            {"name": "Chimpanzee", "selected": false},
+            {"name": "Crocodile", "selected": false},
+            {"name": "Flamingo", "selected": false},
+            {"name": "Peacock", "selected": false},
+            {"name": "Jaguar", "selected": false},
+            {"name": "Leopard", "selected": false},
+            {"name": "Wolf", "selected": false},
+            {"name": "Fox", "selected": false},
+            {"name": "Bald Eagle", "selected": false},
+            {"name": "Owl", "selected": false},
+            {"name": "Frog", "selected": false},
+            {"name": "Shark", "selected": false},
+            {"name": "Octopus", "selected": false},
+            {"name": "Turtle", "selected": false},
+            {"name": "Snake", "selected": false}
+        ],
+
+- how will i randomize the selected category for a selected word ? 
+
+i can could get the total items in the array multiply it by math.random() and then return a random word or just use a randomizer algorithm (considering big 0 notation)
+
+1. Should there be a state for the secret word ? 
+
+yes when we get a secret word we can update it here
+
+2. Should there be a state for the selected alphabet word ? 
+maybe
+
+3. should there be a state for the category selected ?
+yes it will be useful
+
+4. How do we track the health ? 
+okay we make a win or loose state or tracker, if the selected alphabet is in the secret word it will not subtract a health but if it is not
+in the selected alphabet it will subtract from their health
+
+5. How will I handle the alphabets to be displayed on the UI
+render an alpahent array from a -z 
+
+
+6. How will I handle the alphabets when an alphabet is selected
+- if the alphabet selected appears twice in the word how will i handle that ? 
+i will compare it
+- if the alphabet is not the word how will i handle that 
+- ignore it and delete a health point
+
+revelation state: ["B", "A", "_", "A", "_", "A"]
+7. If the user selects an alphabet and it is in the secret word how will i display that
+so i wil compare the selected alphabet to the whole word and return the position that alphabet might be in (also counting spaces!!)
+revelation state: ["B", "A", "_", "A", "_", "A"]
+
 */
 
-const GameContext = createContext();
+interface GameState {
+  secretWord: string;
+  setSecretWord: Dispatch<SetStateAction<string>>;
+  category: string;
+  selectedCategory: Dispatch<SetStateAction<string>>;
+  showMenu: boolean;
+  setShowMenu: Dispatch<SetStateAction<boolean>>;
+  gameStatus: "menu" | "playing" | "won" | "lost";
+  health: number;
+  guessedLetters: string[];
+  revealedWord: string[];
+}
 
-export const GameProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  const [secretWord, setSecretWord] = useState<String>("hII");
-  const [category, selectedCategory] = useState<String>("");
+interface GameActionProps {
+  children: ReactNode;
+  selectAlphabet: (letter: string) => void;
+  playGame: () => void;
+  pauseGame: () => void;
+  reduceHealthPoints: () => void;
+  playAgain: () => void;
+  continueGame: () => void;
+  newCategory: () => void;
+  quitGame: () => void;
+}
+
+type WordData = { name: string; selected: boolean; category: string };
+
+const GameContext = createContext<GameState | undefined>(undefined);
+
+export const GameProvider = ({ children }: GameActionProps) => {
+  const [secretWord, setSecretWord] = useState<string>("");
+  const [category, selectedCategory] = useState<string>("");
+  const [showMenu, setShowMenu] = useState<boolean>(false);
 
   return (
     <GameContext.Provider
-      value={{ secretWord, setSecretWord, category, selectedCategory }}
+      value={{
+        secretWord,
+        setSecretWord,
+        category,
+        selectedCategory,
+        showMenu,
+        setShowMenu,
+      }}
     >
       {children}
     </GameContext.Provider>
@@ -38,7 +174,7 @@ export const GameProvider: React.FC<{ children: React.ReactNode }> = ({
 export const useGameContext = () => {
   const context = useContext(GameContext);
   if (context === undefined)
-    throw new Error("Wrap your app with the Game Provider please!");
+    throw new Error("Kindly Wrap your app with the Game Provider please!");
 
   return context;
 };
